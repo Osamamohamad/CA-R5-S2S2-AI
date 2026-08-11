b27d570cb6317f5f307395d97167320c20513a42
Osama Mohammed <o.s.m.ohammad101@gmail.com>
2026-08-11 07:34:41 +0300

    Add Session 4 assignment

---
+++ src/python/Session-4/assignments/main.py
@@ -0,0 +1,17 @@
+from math1 import calculator , MultiplicationTable , TwinPrimes , PrimeFactors , decBin , PerfectNum
+
+# calculator()
+
+# MultiplicationTable()
+
+# TwinPrimes()
+
+# Num=int(input("Enter integar number : "))
+# print(PrimeFactors(Num))
+
+# Num=int(input("Enter decimal number : "))
+# print(decBin(Num))
+
+start=int(input("Enter the start of the range : "))
+end=int(input("Enter the end of the range : "))
+print(PerfectNum(start,end))
