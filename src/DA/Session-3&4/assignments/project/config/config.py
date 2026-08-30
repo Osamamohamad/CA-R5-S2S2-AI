0f70049a798c26e4f7bedafb16b3bc688682a1c3
Osama Mohammed <o.s.m.ohammad101@gmail.com>
2026-08-30 03:37:28 +0300

    Add preprocessing project structure and pipeline files

---
+++ src/DA/Session-3&4/assignments/project/config/config.py
@@ -0,0 +1,4 @@
+import os
+
+DATA_PATH = "C:/Users/11/OneDrive - Future University in Egypt/Desktop/DEPI/CA-R5-S2S2-AI/src/DA/Session-3&4/assignments/project/data/raw/Titanic.xlsx"
+COLS_TO_DROP = ["PassengerId", "Name", "Ticket", "Cabin"]
