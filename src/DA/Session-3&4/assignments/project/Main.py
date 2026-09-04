58825d67536da3fc8cf08e119cb5b9e5a6f66c49
Osama Mohammed <o.s.m.ohammad101@gmail.com>
2026-09-04 16:19:27 +0300

    Add Session 5 DA and data preprocessing assignment

--- src/DA/Session-3&4/assignments/project/Main.py
+++ src/DA/Session-3&4/assignments/project/Main.py
@@ -5,14 +5,14 @@ def main():
     df = read_data_file(DATA_PATH)
     
     if df is not None:
-        print("-----> Data Summary Before Cleaning <-----")
+        print("-----> Data Summary Before Cleaning <----osama-")
         summary_before = check_data_type(df)
         print(summary_before)
-        print("\n" + "="*50 + "\n")
+        print("osama\n" + "="*50 + "\n")
         
         df_cleaned = drop_unnecessary_features(df, COLS_TO_DROP)
         
-        print("-----> Data Summary After Dropping Features <-----")
+        print("-----> Data Summary After Dropping Features <----osama-")
         summary_after = check_data_type(df_cleaned)
         print(summary_after)
 
